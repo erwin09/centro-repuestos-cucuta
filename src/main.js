ffc59b8f4fac58dafa716d7874567e95a840cc26
Edinson <eantolinez@outlook.com>
2025-06-12 18:11:15 -0500

    define store persist

--- src/main.js
+++ src/main.js
@@ -9,6 +9,35 @@ axios.defaults.baseURL = BASE_URL;
 
 const pinia = createPinia();
 
+// mantener estado persistente en localstorage
+pinia.use(({ store }) => {
+  store.$router = markRaw(router);
+  const serializer = {
+    serialize: JSON.stringify,
+    deserialize: JSON.parse,
+  };
+
+  const fromStorage = serializer.deserialize(
+    window.localStorage.getItem("piniaState")
+  );
+
+  if (fromStorage) store.$patch(fromStorage);
+
+  store.$subscribe(
+    (mutation, state) => {
+      window.localStorage.setItem(
+        "state",
+        serializer.serialize(
+          state.hasOwnProperty("storeApp") ? state.storeApp : state
+        )
+      );
+    },
+    {
+      detached: true,
+    }
+  );
+});
+
 const app = createApp(App);
 //app.use(interceptor);
 app.use(pinia);
